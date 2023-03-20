b5b3d5fb028c3925e62a67acafd7b674e5651002
Matheus <matheushbgois@gmail.com>
2023-03-19 23:38:36 -0300

    commiiittt

--- script.js
+++ script.js
@@ -5,10 +5,12 @@ function submitForm() {
   const experience = document.getElementById("experience").value;
   const salary = document.getElementById("salary").value;
 
-  if (married == 1 && age >= 35 && age <= 50 && education == 1 && experience >= 5 && salary >= 2500 && salary <= 3000) {
-    document.getElementById("resultMessage").innerHTML = "Candidato selecionado!";
+  if (married == 1 && age >= 18 && age <= 22 && ((education == 1 && experience >= 5 && salary >= 2500 && salary <= 3000) || (education == 2 && married == 1))) {
+      document.getElementById("resultMessage").innerHTML = "Candidato selecionado!";
+      document.getElementById("resultMessage").style.color = "green";
   } else {
-    document.getElementById("resultMessage").innerHTML = "Candidato não selecionado.";
+      document.getElementById("resultMessage").innerHTML = "Candidato não selecionado.";
+      document.getElementById("resultMessage").style.color = "red";
   }
 }
 
